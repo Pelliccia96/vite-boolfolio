d9498e1d5db2717869f8a29716000a8cc0e58cd2
Pelliccia96 <fra.pelliccia96@gmail.com>
2023-02-16 15:15:37 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <TheMain></TheMain>
+</template>
+
+<script>
+import TheMain from './components/TheMain.vue';
+
+  export default {
+    components: { TheMain }
+}
+</script>
+
+<style lang="scss">
+  @use "styles/main";
+</style>
---
+++ src/components/TheMain.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container my-5">
+        <div class="text-center">
+            <h1 class="text-white">{{ title }}</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    data() {
+        return {
+            title: "Vite Boolfolio",
+        }
+    },
+  }
+</script>
+
+<style scoped>
+</style>
